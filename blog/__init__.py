37820bfb03eb70fd178c4723b03c0c099fcc951b
RudeZwloki7 <rudezwloki7@gmail.com>
2022-01-21 16:17:19 +0300

    Add templates and some routes

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pgpass@localhost/blog_db'
 app.config['SECRET_KEY'] = 'test_cism_2022'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login_page'
+# login_manager.login_message_category = 'info'
 
 from blog import routes
--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,14 @@
 from blog import app
+from blog import db
+from flask import render_template, redirect, url_for, flash, request
+
+
+@app.route('/info')
+def info_page():
+    return render_template('info.html')
 
 
 @app.route('/')
-@app.route('/home')
-def home_page():
-    return '<p>Hello World</p>'
+@app.route('/feed')
+def feed_page():
+    return render_template('feed.html')
